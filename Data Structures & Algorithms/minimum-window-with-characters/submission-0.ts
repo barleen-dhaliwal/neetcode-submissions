e257327160e738e31bfccad47856ce05ee0abf3d
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-05-12 17:53:24 -0700

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.ts
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s: string, t: string): string {
+
+        if (t.length > s.length)
+            return "";
+
+        let tFreq=new Map<string,number>();
+        for(let i=0;i<t.length;i++){
+            let f=tFreq.get(t[i])||0;
+            tFreq.set(t[i],f+1);
+        }
+        let required = tFreq.size; // number of unique chars needed
+        let formed = 0;
+
+        let windowFreq=new Map<string,number>();
+        let l=0;
+        let minLen=Infinity;
+        let startIndex=-1;
+        for(let r=0;r<s.length;r++){
+            let f=windowFreq.get(s[r])||0;
+            windowFreq.set(s[r],f+1);
+
+             // if this character now satisfies requirement
+            if (
+                tFreq.has(s[r]) &&
+                windowFreq.get(s[r]) === tFreq.get(s[r])
+            ) {
+                formed++;
+            }
+
+            while(formed===required){
+                if(minLen>r-l+1){
+                    minLen=r-l+1;
+                    startIndex=l;
+                }
+                windowFreq.set(s[l],windowFreq.get(s[l])-1);
+                if(windowFreq.get(s[l])<tFreq.get(s[l])){
+                    formed--;
+                }
+                l++;
+
+            }
+
+        }
+        return startIndex===-1?"":s.substring(startIndex,startIndex+minLen);
+    }
+}
