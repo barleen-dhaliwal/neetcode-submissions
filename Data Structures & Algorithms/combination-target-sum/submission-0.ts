e669682c40e1bf4780c786dee3142e31ab882845
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-05-19 12:09:09 -0700

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums: number[], target: number): number[][] {
+
+        const ans=[];
+
+        const dfs=(i:number,curr: number[],total:number)=>{
+
+            if(total===target)
+            {
+                ans.push(curr);
+                return;
+            }
+            if(i>nums.length-1||total>target)
+                return;
+            
+            dfs(i+1,[...curr],total)
+            curr.push(nums[i])
+            dfs(i,[...curr],total+nums[i])
+
+        }
+
+        dfs(0,[],0);
+        return ans;
+    }
+}
