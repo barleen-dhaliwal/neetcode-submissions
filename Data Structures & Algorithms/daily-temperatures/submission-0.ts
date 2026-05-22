a68d83b4e18efce050750a387b2c77dcd982c8f3
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-05-22 13:47:14 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+
+        const stack:{val:number, index:number}[]=[];
+        const result=new Array(temperatures.length).fill(0);
+
+        for(const [index,temp] of temperatures.entries()){
+
+            if(!stack.length || stack[stack.length-1].val>=temp)
+                stack.push({val:temp,index})
+            else{
+                while(stack.length && stack[stack.length-1].val<temp){
+
+                    const poppedElement=stack.pop();
+                    result[poppedElement.index]=index-poppedElement.index;
+
+                }
+                stack.push({val:temp,index});
+            }
+        }
+
+        return result;
+
+    }
+}
