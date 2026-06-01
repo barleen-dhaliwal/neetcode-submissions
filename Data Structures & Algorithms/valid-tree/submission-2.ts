850f81d26c89952358daf236cf6fcc987c4e1c5b
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-06-01 14:36:54 -0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n: number, edges: number[][]): boolean {
+        const adjList = new Map();
+
+        for (let i = 0; i < n; i++) {
+            adjList.set(i, []);
+        }
+        for (const [node, neighbour] of edges) {
+            adjList.get(node).push(neighbour);
+            adjList.get(neighbour).push(node);
+        }
+
+        const visited = new Set();
+
+        const dfs = (node: number, prev: number) => {
+            if (visited.has(node)) {
+                return false;
+            }
+            visited.add(node);
+            for (const neighbour of adjList.get(node)) {
+                if (neighbour !== prev && !dfs(neighbour, node)) return false;
+            }
+            return true;
+        };
+        if (!dfs(0, -1)) return false;
+        return visited.size === n;
+    }
+}
