8f3dbe255c361d407a5925768574e5dada1d073a
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-02 15:43:22 -0700

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.ts
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums: number[]): number {
+
+        nums.sort();
+         return nums[Math.floor(nums.length/2)]
+    }
+}
