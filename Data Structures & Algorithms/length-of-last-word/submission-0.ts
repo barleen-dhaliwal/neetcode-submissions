c7f6d9f12c9cdfebc46a5c2ffd5a028559977929
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-02 14:03:05 -0700

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLastWord(s: string): number {
+        s=s.trim();
+        let count=0;
+        for(let i=s.length-1;i>=0;i--){
+            if(s[i]===' ')
+                return count;
+            count++;
+        }
+        return count;
+    }
+}
