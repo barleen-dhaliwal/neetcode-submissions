c9993f1d4fee025e31525971f89bdf8b01b41494
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-02 14:17:53 -0700

    Add: string-matching-in-an-array - submission-1

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-1.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {string[]}
+     */
+    stringMatching(words: string[]): string[] {
+        const ans=[];
+
+        for(let i=0;i<words.length;i++){
+            
+            for(let j=0;j<words.length;j++){
+                
+                if(i!==j && words[j].includes(words[i])){
+                    ans.push(words[i])
+                    break;
+                }
+
+            }
+
+        }
+
+        return ans;
+
+    }
+}
