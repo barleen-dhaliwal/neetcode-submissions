6af9754c86409de436ca6b962d98dd30ad01221c
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-02 13:38:17 -0700

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs: string[]): string {
+       strs.sort();
+
+       for(let i=0;i<strs[0].length;i++){
+            if(strs[0][i]!==strs[strs.length-1][i])
+                return strs[0].substring(0,i)
+
+       }
+
+       return strs[0]
+
+    }
+}
