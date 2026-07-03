eb2b9ee25d5d5703e6985eca8b0caa86a06d7c5d
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-03 15:01:52 -0700

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-2

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-2.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} haystack
+     * @param {string} needle
+     * @return {number}
+     */
+    strStr(haystack: string, needle: string): number {
+       for(let i=0;i<=haystack.length-needle.length;i++){
+            if(haystack.substring(i,i+needle.length)===needle)
+                return i;
+
+       }
+       return -1;
+    }
+}
