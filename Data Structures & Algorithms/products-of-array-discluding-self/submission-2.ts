f18b1d09f07cbae64ea3d260bab422ddb044b621
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-03 12:20:40 -0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const res = new Array(nums.length).fill(1);
+
+        for (let i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        let product = 1;
+        for (let i = nums.length - 1; i >= 0; i--) {
+            res[i] = res[i] * product;
+            product = product * nums[i];
+        }
+
+        return res;
+    }
+}
