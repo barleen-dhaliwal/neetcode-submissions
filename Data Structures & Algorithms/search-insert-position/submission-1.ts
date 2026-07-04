da8b22599ce64da5ba33c4d8e636fd764a0c8e63
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-04 08:41:01 -0700

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            let mid = Math.floor((l + r) / 2);
+            if (nums[mid] === target) return mid;
+            if (target > nums[mid]) l = mid + 1;
+            else r = mid - 1;
+        }
+        return l;
+    }
+}
