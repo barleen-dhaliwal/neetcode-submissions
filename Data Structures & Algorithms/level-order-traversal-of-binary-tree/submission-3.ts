a9a3567ba98090c61fc21a98788a51993cf535fc
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-05 21:15:47 -0700

    Add: level-order-traversal-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-3.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[][]}
+     */
+    levelOrder(root: TreeNode | null): number[][] {
+        const ans = [];
+        if (!root) return ans;
+
+        const q = [root];
+
+        while (q.length) {
+            ans.push(q.map((e) => e.val));
+            const next = [];
+            while (q.length) {
+                const element = q.shift();
+                if (element.left) next.push(element.left);
+                if (element.right) next.push(element.right);
+            }
+            q.push(...next);
+        }
+
+        return ans;
+    }
+}
