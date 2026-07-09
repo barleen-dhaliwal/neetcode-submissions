652450513547f0f596e187bdb2b11fb8689346ca
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-09 09:15:20 -0700

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        const ans = [];
+
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+            let j = i + 1;
+            let k = nums.length - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] === -nums[i]) {
+                    ans.push([nums[i], nums[j], nums[k]]);
+                    j++;
+                    k--;
+                    // skip duplicate left value
+                    while (j < k && nums[j] === nums[j - 1]) j++;
+                    // skip duplicate right value
+                    while (j < k && nums[k] === nums[k + 1]) k--;
+                } else if (nums[j] + nums[k] + nums[i] > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
