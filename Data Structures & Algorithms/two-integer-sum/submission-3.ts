3c7a72601a9a4cbdeeec8e8d8a30a223a3a36483
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-07-08 20:03:41 -0700

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+
+        const map=new Map<number,number>();
+
+        for(let i=0;i<nums.length;i++){
+            const index=map.get(target-nums[i])
+            if(index!==undefined){
+                return [index,i]
+            }
+            map.set(nums[i],i)
+        }
+
+    }
+}
