fffffa704d181fa912b6cc5c0d9bdfd7cd2025af
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-08-22 06:43:02 -0700

    Add: reorder-linked-list - submission-9

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-9.ts
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head: ListNode | null): void {
+        if (!head || !head.next) return;
+
+        //Find end of first half
+        let slow = head;
+        let fast = head.next;
+        while (fast && fast.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        let p1 = head;
+        let p2 = this.reverseList(slow.next);
+        slow.next=null;
+
+        while (p1 && p2) {
+            const p1Next = p1.next;
+            const p2Next = p2.next;
+            p1.next = p2;
+            p2.next=p1Next
+            p1 = p1Next;
+            p2 = p2Next;
+        }
+    }
+
+    reverseList(head: ListNode) {
+        let prev = null;
+        let curr = head;
+        while (curr) {
+            const next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
