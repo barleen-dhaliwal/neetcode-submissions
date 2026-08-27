4ac331e4723faf331a2029b38bd04620663a70c5
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-08-27 16:25:37 -0700

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval  {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals: Interval[]): boolean {
+        intervals.sort((a, b) => a.start - b.start);
+
+        let prev = intervals[0];
+
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < prev.end) return false;
+            prev = intervals[i];
+        }
+        return true;
+    }
+}
