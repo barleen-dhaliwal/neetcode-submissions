5107919fb6b8bc48dea8eab681fd6ee3c0c03dcc
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-08-27 09:47:05 -0700

    Add: intersection-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-1.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    intersection(nums1: number[], nums2: number[]): number[] {
+        const set = new Set<number>(nums1);
+        const ans = new Set<number>()
+        for (const n of nums2) {
+            if (set.has(n)) ans.add(n);
+        }
+        return [...ans]
+    }
+}
