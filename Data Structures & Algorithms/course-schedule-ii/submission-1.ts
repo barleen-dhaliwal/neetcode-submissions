faa0e38ded7145f00f91743d639934a7c9c01f24
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-09-02 13:17:00 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.ts
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {number[]}
+     */
+    findOrder(numCourses: number, prerequisites: number[][]): number[] {
+        const adjList = new Map<number, number[]>();
+
+        for (let i = 0; i < prerequisites.length; i++) {
+            if (!adjList.has(prerequisites[i][0])) adjList.set(prerequisites[i][0], []);
+            adjList.get(prerequisites[i][0]).push(prerequisites[i][1]);
+        }
+
+        const visited = new Set();
+        const visiting = new Set();
+
+        const ans = [];
+
+        const dfs = (node: number) => {
+            if (visited.has(node)) return true;
+
+            if (visiting.has(node)) return false;
+
+            const neighbours = adjList.get(node) || [];
+
+            visiting.add(node);
+            for (let i = 0; i < neighbours.length; i++) {
+                if (!dfs(neighbours[i])) return false;
+            }
+
+            visiting.delete(node);
+            visited.add(node);
+            ans.push(node);
+            return true;
+        };
+
+        for (let i = 0; i < numCourses; i++) {
+            if (!dfs(i)) return [];
+        }
+
+        return ans;
+    }
+}
