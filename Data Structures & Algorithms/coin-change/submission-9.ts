59fbb9b6238edf22df4f25f2d934365b1e034324
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-09-04 13:33:32 -0700

    Add: coin-change - submission-9

---
+++ Data Structures & Algorithms/coin-change/submission-9.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins: number[], amount: number): number {
+        const map = new Map<number, number>();
+
+        return this.helper(coins, amount, map);
+    }
+    
+    helper(coins: number[], amount: number, map: Map<number, number>) {
+        if (amount < 0) return -1;
+        if (amount === 0) return 0;
+
+        let minCoins = +Infinity;
+        for (let i = 0; i < coins.length; i++) {
+            const res = map.get(amount - coins[i]) ?? this.helper(coins, amount - coins[i], map);
+            if (res !== -1) {
+                minCoins = Math.min(1 + res, minCoins);
+            }
+        }
+
+        const ans = minCoins === Infinity ? -1 : minCoins;
+
+        map.set(amount, ans);
+
+        return ans;
+    }
+}
