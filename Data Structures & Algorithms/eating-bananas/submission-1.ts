5029c03b9c072f35614717a4ca76da5e69f79e9e
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-09-06 11:46:52 -0700

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles: number[], h: number): number {
+        let left = 1;
+        let right = Math.max(...piles);
+
+        while (left < right) {
+            const mid = Math.floor((left + right) / 2);
+            if (this.canFinish(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+
+    canFinish(piles: number[], rate: number, h: number) {
+        let i = 0;
+        while (i < piles.length) {
+            const hours = Math.ceil(piles[i] / rate);
+            h = h - hours;
+            if (h < 0) return false;
+
+            i++;
+        }
+
+        return true;
+    }
+}
