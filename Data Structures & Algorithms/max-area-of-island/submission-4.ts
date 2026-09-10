b4caa202f9fbc3e2938d525839924221d868fa6a
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-09-10 08:53:18 -0700

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.ts
@@ -0,0 +1,42 @@
+const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+];
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid: number[][]): number {
+        let ans = 0;
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] === 1) {
+                    ans = Math.max(ans, this.getArea(grid, i, j));
+                }
+            }
+        }
+        return ans;
+    }
+
+    getArea(grid: number[][], row: number, col: number) {
+        if (
+            row < 0 ||
+            col < 0 ||
+            row === grid.length ||
+            col === grid[0].length ||
+            grid[row][col] === 0
+        )
+            return 0;
+
+        grid[row][col] = 0;
+
+        let area = 1;
+        for (const [dr, dc] of directions) {
+            area += this.getArea(grid, row + dr, col + dc);
+        }
+        return area;
+    }
+}
