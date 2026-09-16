2d48ac34e644b509169f19e54f89688edebc0adf
barleen-dhaliwal <55175038+barleen-dhaliwal@users.noreply.github.com>
2026-09-16 16:31:45 -0700

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m: number, n: number): number {
+        const dp = new Array(n).fill(1);
+        for (let i = 1; i < m; i++) {
+            for (let j = 1; j < n; j++) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
